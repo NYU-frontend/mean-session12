b56085cf397e32d616f813d5449a3d6b43e2198a
DannyBoyNYC <daniel.deverell@gmail.com>
2015-08-24 10:14:07 -0400

    first commit

---
+++ _review-files/1b-code-routing-templates-controllers/done/app.js
@@ -0,0 +1,56 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+});
+
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+
+    $scope.name = 'Main';
+
+    $log.main = 'property from main';
+    $log.log($scope);
+    
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
+
+    $scope.name = 'Second';
+
+    $log.second = 'property from second';
+    $log.log($scope);
+
+    
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
